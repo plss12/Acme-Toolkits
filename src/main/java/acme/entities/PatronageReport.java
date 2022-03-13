6df7d913799422255403d0579df67fecffdf096d
alvmorcal1 <alvmorcal1@alum.us.es>
2022-03-13 19:57:50 +0100

    Task 029:Create relation "patronage report": Initial Version
    Is not the final version, missing the "automatic sequence number".

---
+++ src/main/java/acme/entities/PatronageReport.java
@@ -0,0 +1,59 @@
+package acme.entities;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.URL;
+
+import acme.framework.entities.AbstractEntity;
+import acme.roles.Inventor;
+import acme.roles.Patron;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class PatronageReport extends AbstractEntity{
+	
+	// Serialisation identifier -----------------------------------------------
+
+	protected static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	@Past
+	protected Date creationMoment;
+	
+	@NotBlank
+	@Size(max=256)
+	protected String memorandum;
+	
+	@URL
+	protected String link;
+	
+	// Derived attributes -----------------------------------------------------	
+
+	// Relationships ----------------------------------------------------------	
+
+	@NotNull
+	@Valid
+	@ManyToOne(optional = false)
+	protected Inventor inventor;
+	
+	@NotNull
+	@Valid
+	@ManyToOne(optional = false)
+	protected Patron patron;
+	
+}
