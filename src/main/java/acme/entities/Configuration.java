d326f8017be853a8ff2acda65adf1a8fcff6423c
pedsotsan <pedsotsan@alum.us.es>
2022-03-18 16:21:40 +0100

    Task-036: Fixed

--- src/main/java/acme/entities/Configuration.java
+++ src/main/java/acme/entities/Configuration.java
@@ -1,5 +1,7 @@
 package acme.entities;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotNull;
@@ -24,12 +26,18 @@ public class Configuration extends AbstractEntity{
 	@NotNull
 	protected String defaultCurrency;
 	
+	protected List<String> acceptedCurrencies;
+	
 	@Range(min=0, max=1)
-	@Digits(integer=1, fraction=2)
+	@Digits(integer=2, fraction=0)
 	protected double weakSpamTrheshold;
 	
 	@Range(min=0, max=1)
-	@Digits(integer=1, fraction=2)
+	@Digits(integer=2, fraction=0)
 	protected double strongSpamTrheshold;
+	
+	protected List<String> strongSpam;
+	
+	protected List<String> weakSpam;
 	
 }
