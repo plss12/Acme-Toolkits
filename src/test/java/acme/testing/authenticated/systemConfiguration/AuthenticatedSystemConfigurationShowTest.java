c79b8d96cf89b291939432ac80f87b3e52ace59b
salparram <salparram@alum.us.es>
2022-04-19 23:47:23 +0200

    Task-043(Test): POSITIVE TEST DONE

---
+++ src/test/java/acme/testing/authenticated/systemConfiguration/AuthenticatedSystemConfigurationShowTest.java
@@ -0,0 +1,26 @@
+package acme.testing.authenticated.systemConfiguration;
+
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import acme.testing.TestHarness;
+
+public class AuthenticatedSystemConfigurationShowTest extends TestHarness{
+	
+	//Test cases
+	
+		@ParameterizedTest
+		@CsvFileSource(resources = "/authenticated/system-configuration/show.csv", encoding = "utf-8", numLinesToSkip = 1)
+		@Order(10)	
+		public void positiveTest(final int recordIndex, final String defaultCurrency, final String acceptedCurrencies) {
+			
+			super.signIn("administrator", "administrator");
+			super.clickOnMenu("Authenticated", "System Configuration");
+			super.checkFormExists();
+			super.checkInputBoxHasValue("acceptedCurrencies", acceptedCurrencies);
+			super.checkInputBoxHasValue("defaultCurrency", defaultCurrency);
+			super.signOut();
+		}
+
+}
