3591e74a755dc4722d36a392c3756f0687895b93
pepol <pepol@LAPTOP-C41EFLVK>
2022-04-23 15:01:34 +0200

    Add new relations

--- src/main/java/acme/entities/ArtifactToolkit.java
+++ src/main/java/acme/entities/ArtifactToolkit.java
@@ -1,6 +1,9 @@
 package acme.entities;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 
 import acme.framework.entities.AbstractEntity;
 import lombok.Getter;
@@ -19,6 +22,14 @@ public class ArtifactToolkit extends AbstractEntity{
 	
 	protected int artifactAmount;
 	
-	protected int toolkitAmount;
+	@NotNull
+	@Valid
+	@ManyToOne(optional=false)
+	protected Toolkit toolkit;
+	
+	@NotNull
+	@Valid
+	@ManyToOne(optional=false)
+	protected Artifact artifact;
 
 }
