9914b9554b45764280a0864bc622bebfde4c598a
alvmorcal1 <alvmorcal1@alum.us.es>
2022-04-24 19:08:25 +0200

    Administrator Dashboard Done.

--- src/main/java/acme/forms/AdministratorDashboard.java
+++ src/main/java/acme/forms/AdministratorDashboard.java
@@ -19,24 +19,24 @@ public class AdministratorDashboard implements Serializable{
 
 	// Attributes -------------------------------------------------------------
 	
-	int										totalNumberOfComponents;
-	Map<Pair<String, String>, Double>		averageRetailPriceOfComponents;
-	Map<Pair<String, String>, Double>		retailPriceDeviationOfComponents;
-	Map<Pair<String, String>, Double>		minimumRetailPriceOfComponents;
-	Map<Pair<String, String>, Double>		maximumRetailPriceOfComponents;
+	int											totalNumberOfComponents;
+	Map<Pair<String, String>, Double>			averageRetailPriceOfComponents;
+	Map<Pair<String, String>, Double>			retailPriceDeviationOfComponents;
+	Map<Pair<String, String>, Double>			minimumRetailPriceOfComponents;
+	Map<Pair<String, String>, Double>			maximumRetailPriceOfComponents;
 	
-	int										totalNumberOfTools;
-	Map<String, Double>						averageRetailPriceOfTools;
-	Map<String, Double>						retailPriceDeviationOfTools;
-	Map<String, Double>						minimumRetailPriceOfTools;
-	Map<String, Double>						maximumRetailPriceOfTools;
+	int											totalNumberOfTools;
+	Map<String, Double>							averageRetailPriceOfTools;
+	Map<String, Double>							retailPriceDeviationOfTools;
+	Map<String, Double>							minimumRetailPriceOfTools;
+	Map<String, Double>							maximumRetailPriceOfTools;
 	
 	
-	Map<PatronageStatus, Long>				totalNumberOfPatronages;
-	Map<PatronageStatus, Double>			averageBudgetOfPatronages;
-    Map<PatronageStatus, Double>			deviationBudgetOfPatronages;
-	Map<PatronageStatus, Double>			minimumBudgetOfPatronages;
-	Map<PatronageStatus, Double>			maximumBudgetOfPatronages;
+	Map<PatronageStatus, Long>					totalNumberOfPatronages;
+	Map<Pair<PatronageStatus,String>,Double>	averageBudgetOfPatronages;
+	Map<Pair<PatronageStatus,String>,Double>	deviationBudgetOfPatronages;
+	Map<Pair<PatronageStatus,String>,Double>	minimumBudgetOfPatronages;
+	Map<Pair<PatronageStatus,String>,Double>	maximumBudgetOfPatronages;
 	
 	// Derived attributes -----------------------------------------------------
 
