861c2abadcaeb9390e136e4590590715feb453d4
juacabvil <juacabvil@alum.us.es>
2022-05-10 12:33:39 +0200

    Task 068: Done

---
+++ src/main/java/acme/features/administrator/annoucement/AdministratorAnnouncementController.java
@@ -0,0 +1,31 @@
+package acme.features.administrator.annoucement;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import acme.entities.Announcement;
+import acme.framework.controllers.AbstractController;
+import acme.framework.roles.Administrator;
+
+@Controller
+
+public class AdministratorAnnouncementController extends AbstractController<Administrator, Announcement>{
+	// Internal state ---------------------------------------------------------
+
+
+		@Autowired
+		protected AdministratorAnnouncementCreateService		createService;
+
+
+		// Constructors -----------------------------------------------------------
+
+
+		@PostConstruct
+		protected void initialise() {
+			super.addCommand("create", this.createService);
+		}
+	
+
+}
