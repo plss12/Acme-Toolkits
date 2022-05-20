ab9624aca9f7a5e5eee1dafe75dea700c90c8421
alvmorcal1 <alvmorcal1@alum.us.es>
2022-05-20 16:52:38 +0200

    Task 077: Done test in Patronages.

---
+++ src/test/java/acme/testing/patron/patronage/PatronPatronageUpdateTest.java
@@ -0,0 +1,93 @@
+package acme.testing.patron.patronage;
+
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import acme.testing.TestHarness;
+
+public class PatronPatronageUpdateTest extends TestHarness{
+
+	@ParameterizedTest
+	@CsvFileSource(resources = "/patron/patronage/update-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(10)
+	public void positiveTest(final int recordIndex, final String code, final String legalStuff, final String budget,
+		final String startDate, final String finishDate, final String link, final String inventorSelector) {
+		super.signIn("patron1", "patron1");
+		
+		super.clickOnMenu("Patron", "List my Patronages");
+		super.checkListingExists();
+		super.sortListing(0, "asc");
+		super.clickOnListingRecord(recordIndex);
+		super.checkFormExists();
+		
+		super.fillInputBoxIn("code", code);
+		super.fillInputBoxIn("legalStuff", legalStuff);
+		super.fillInputBoxIn("budget", budget);
+		super.fillInputBoxIn("startDate", startDate);
+		super.fillInputBoxIn("finishDate", finishDate);
+		super.fillInputBoxIn("link", link);
+		super.fillInputBoxIn("inventor.userAccount.username", inventorSelector);
+		
+		super.clickOnSubmit("Update");
+		
+		super.clickOnMenu("Patron", "List my Patronages");
+		super.checkListingExists();
+		super.sortListing(0, "desc");
+		super.checkColumnHasValue(recordIndex, 0, code);
+		super.checkColumnHasValue(recordIndex, 1, startDate);
+		super.checkColumnHasValue(recordIndex, 2, finishDate);
+		super.checkColumnHasValue(recordIndex, 3, legalStuff);
+		super.checkColumnHasValue(recordIndex, 4, "PROPOSED");
+		super.clickOnListingRecord(recordIndex);
+		
+		super.checkFormExists();
+		super.checkInputBoxHasValue("code", code);
+		super.checkInputBoxHasValue("legalStuff", legalStuff);
+		super.checkInputBoxHasValue("budget", budget);
+		super.checkInputBoxHasValue("startDate", startDate);
+		super.checkInputBoxHasValue("finishDate", finishDate);
+		super.checkInputBoxHasValue("link", link);
+		
+		super.clickOnButton("Patronage Reports");
+		
+		super.checkListingExists();
+		super.checkNotListingEmpty();
+		
+		super.clickOnMenu("Patron", "List my Patronages");
+		super.checkListingExists();
+		super.sortListing(0, "desc");
+		super.clickOnListingRecord(recordIndex);
+		
+		super.signOut();		
+	}
+	
+	@ParameterizedTest
+	@CsvFileSource(resources = "/patron/patronage/update-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(20)
+	public void negativeTest(final int recordIndex, final String code, final String legalStuff, final String budget,
+		final String startDate, final String finishDate, final String link, final String inventorSelector) {
+		
+		super.signIn("patron1", "patron1");
+		
+		super.clickOnMenu("Patron", "List my Patronages");
+		super.checkListingExists();
+		super.sortListing(0, "desc");
+		super.clickOnListingRecord(recordIndex);
+		super.checkFormExists();
+	
+		super.fillInputBoxIn("code", code);
+		super.fillInputBoxIn("legalStuff", legalStuff);
+		super.fillInputBoxIn("budget", budget);
+		super.fillInputBoxIn("startDate", startDate);
+		super.fillInputBoxIn("finishDate", finishDate);
+		super.fillInputBoxIn("link", link);
+		super.fillInputBoxIn("inventor.userAccount.username", inventorSelector);
+		
+		super.clickOnSubmit("Update");
+		
+		super.checkErrorsExist();
+		
+		super.signOut();
+	}
+}
